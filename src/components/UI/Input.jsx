6b0e5b06dae73cffb9392c4bcbc055bcae3a834e
oladapo <oladapoelijah49@gmail.com>
2024-01-31 03:03:29 +0100

    food ordering app

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,10 @@
+const Input = ({ label, id, ...props }) => {
+  return (
+    <p className="control">
+      <label htmlFor={id}>{label}</label>
+      <input {...props} id={id} name={id} required />
+    </p>
+  );
+};
+
+export default Input;
